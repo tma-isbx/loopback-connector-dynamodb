5692cc6788e664583832b4e967d9afd0740de841
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-08-23 15:37:46 -0700

    Add Unit Testings for splitWhere and generateExpression

--- test/expression.js
+++ test/expression.js
@@ -1,10 +1,7 @@
 'use strict';
 
-var should = require('./init.js');
 const assert = require('assert');
-const path = require('path');
 const dynamodb = require('../lib/dynamodb.js');
-const DataSource = require('loopback-datasource-juggler').DataSource;
 
 const index1 = {
   name: 'Test-playerId-born-index1',
@@ -23,43 +20,95 @@ const where1 = {
       { lastName: 'Beckham' }
     ]}
   ]
-}
+};
 
-var config = require('rc')('loopback', {test: {dynamodb: {
-    region: 'local',
-    credentials: 'file',
-    credfile: path.join(__dirname, 'credentials.json'),
-    endpoint: 'http://localhost:8080'
-}}}).test.dynamodb;
+const where2 = {
+  or: [
+    { and: [
+      { key1: 1 },
+      { key2: 2 },
+      { or: [
+        { key3: 3 },
+        { key4: 4 }
+      ]},
+      { key5: 5 }
+    ]},
+    { and: [
+      { or: [
+        { key6: 6 },
+        { key7: 7 }
+      ]},
+      { key8: 8 }
+    ]}
+  ]
+};
 
-describe('Test', () => {
-  describe('sub 1', () => {
-    it('should pass', () => {
-      assert.equal(5, 5);
-    });
+const obj = dynamodb.DynamoDB.prototype.splitWhere(index1, where1);
 
-    it('lets try this', () => {
-      // console.log(dynamodb.initialize);
-      // let what = dynamodb.DynamoDB;
-      // console.log(what);
-      // let obj = dynamodb.splitWhere(index1, where1);
-      //dynamodb.test();
-      //test();
-      // let myDynamodb = dynamodb.initialize({});
-      // console.log(myDynamodb);
-      // var ds = new DataSource(require('../'), config);
-      // dynamodb.initialize(ds, () => {
-      //   let obj = ds.connector.splitWhere(index1, where1);
-      //   console.log(obj.keyQuery);
-      //   assert.notEqual(obj, null);
-      // });
+describe('Testing splitWhere', () => {
+  it('should return object with keyQuery', () => {
+    assert.notEqual(obj.keyQuery, null);
+  });
 
-      let obj = dynamodb.DynamoDB.prototype.splitWhere(index1, where1);
-      console.log(obj.keyQuery);
+  it('should return object with filterQuery', () => {
+    assert.notEqual(obj.filterQuery, null);
+  });
 
-      // let obj = DynamoDB.prototype.splitWhere(index1, where1);
-      
-      // assert.notEqual(obj, null);
-    });
+  it('keyQuery should have the whereObject for keys', () => {
+    assert.equal(obj.keyQuery.playerId, 1);
+    assert.equal(obj.keyQuery.born.between[0], 100);
+    assert.equal(obj.keyQuery.born.between[1], 200);
+  });
+
+  it('filterQuery should have the whereObject for filter', () => {
+    assert.equal(obj.filterQuery.or.length, 3);
+    assert.equal(obj.filterQuery.or[0].height.between[0], 68);
+    assert.equal(obj.filterQuery.or[1].weight.lt, 88);
+    assert.equal(obj.filterQuery.or[2].and[0].team, 'Giants');
+    assert.equal(obj.filterQuery.or[2].and[1].lastName, 'Beckham');
+  })
+});
+
+describe('Testing generateExpression', () => {
+  let params = {};
+  let keyConditionExpression = dynamodb.DynamoDB.prototype.generateExpression(params, obj.keyQuery);
+  let filterExpression = dynamodb.DynamoDB.prototype.generateExpression(params, obj.filterQuery);
+
+  it('should have correct KeyConditionExpression chained with AND', () => {
+    assert.equal(keyConditionExpression, '(#playerId = :1) AND (#born BETWEEN :100 AND :200)');
+  });
+
+  it('should have correct FilterExpression chained with AND and OR', () => {
+    assert.equal(filterExpression, '((#height BETWEEN :68 AND :75) OR (#weight < :88) OR ((#team = :Giants) AND (#lastName = :Beckham)))');
+  });
+
+  it('should have coorect ExpressionAttributeNames with #', () => {
+    assert.equal(params.ExpressionAttributeNames['#playerId'], 'playerId');
+    assert.equal(params.ExpressionAttributeNames['#born'], 'born');
+    assert.equal(params.ExpressionAttributeNames['#height'], 'height');
+    assert.equal(params.ExpressionAttributeNames['#weight'], 'weight');
+    assert.equal(params.ExpressionAttributeNames['#team'], 'team');
+    assert.equal(params.ExpressionAttributeNames['#lastName'], 'lastName');
+  });
+
+  it('should have correct ExpressionAttributeValues with :', () => {
+    assert.equal(params.ExpressionAttributeValues[':1'], 1);
+    assert.equal(params.ExpressionAttributeValues[':100'], 100);
+    assert.equal(params.ExpressionAttributeValues[':200'], 200);
+    assert.equal(params.ExpressionAttributeValues[':68'], 68);
+    assert.equal(params.ExpressionAttributeValues[':75'], 75);
+    assert.equal(params.ExpressionAttributeValues[':88'], 88);
+    assert.equal(params.ExpressionAttributeValues[':Giants'], 'Giants');
+    assert.equal(params.ExpressionAttributeValues[':Beckham'], 'Beckham');
+  });
+
+});
+
+describe('Testing whereObject with complex AND/OR', () => {
+  let params = {};
+  let expression = dynamodb.DynamoDB.prototype.generateExpression(params, where2);
+
+  it('should generate correct Expression with complex AND/OR logic', () => {
+    assert.equal(expression, '(((#key1 = :1) AND (#key2 = :2) AND ((#key3 = :3) OR (#key4 = :4)) AND (#key5 = :5)) OR (((#key6 = :6) OR (#key7 = :7)) AND (#key8 = :8)))');
   });
 });
